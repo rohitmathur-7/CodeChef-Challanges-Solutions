1653ece98eff322f76100946c8deabfdd2a6ac94
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2020-11-19 13:56:13 +0530

    Add files via upload

---
+++ ANDOR - October Lunchtime 2020 Div 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long x;
+	    cin>>x;
+	    cout<<"0"<<" "<<x<<endl;
+	}
+	return 0;
+}
+
